4c3f071a30b1d1d7ce84c3b7ea3fa467517c80b8
gricn <gricn666@gmail.com>
2020-05-06 11:26:59 +0800

    修复testdetail表openid不存在时test_times填写异常bug

--- node/routes/test.js
+++ node/routes/test.js
@@ -5,31 +5,33 @@ const db = require('../db')
 
 test.post('/', async (req, res) => {
     try {
-        console.log(req.body)
         res.send("Server has received test results. Thanks for your paticipating.")
         var reqBody = req.body
         var openid = reqBody.openid
-        var testtimes = reqBody.testtimes
         var question_id = ""
 
-        reqBody.index.forEach((type, i) => {
-            type.forEach((question, j)=>{
-                var tmp1 = i+1
-                var tmp2 = j+1
+        var queryResult = await db.query("select max(test_times) from testdetail \
+            where question_id = 't1_1' and openid= $1", [openid])
+
+        var testtimes = (queryResult == undefined) ? 1 : queryResult.rows[0].max + 1
+
+        await reqBody.index.forEach(async (type, i) => {
+            type.forEach(async (question, j) => {
+                var tmp1 = i + 1
+                var tmp2 = j + 1
                 question_id = "t" + tmp1 + "_" + tmp2
                 question_value = reqBody.index[i][j].sliderValue
-                //question.value
-                const { rows } = db.query(
+
+                await db.query(
                     'insert into testdetail(openid, test_times, question_id, question_value) \
                     values($1, $2, $3, $4)',
                     [openid, testtimes, question_id, question_value])
             })
         })
-
+        console.log('服务器存储testtimes成功，值为：' + testtimes)
     } catch (e) {
         console.log(e)
     }
-
 })
 
 module.exports = test
