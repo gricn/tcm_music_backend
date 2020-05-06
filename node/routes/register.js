e60fd9dd0c668b945f8e6461b4756403b3350982
gricn <gricn666@gmail.com>
2020-05-06 12:00:14 +0800

    修复前端用户注册时发送重复openid导致报错问题

--- node/routes/register.js
+++ node/routes/register.js
@@ -7,8 +7,15 @@ const db = require('../db')
 register.post('/', async (req, res) => {
     try {
         const reqBody = req.body
-        await db.query('insert into userinfo(openid, gender,age,province_id)\
+        const numQuery = await db.query('select count(openid) from userinfo where openid = $1',[reqBody.openid])
+        const openidnum = numQuery.rows[0].count
+
+        if(openidnum == 0){
+            await db.query('insert into userinfo(openid, gender,age,province_id)\
          values ($1,$2,$3,$4)',[reqBody.openid, reqBody.gender, reqBody.age, reqBody.location])
+        }
+        res.send('receive register message successfully.')
+        
     } catch (e) {
         console.log(e)
     }
